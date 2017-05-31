8596a78fe91b1ab58c691aa6e20a7238a8e70db4
pepegar <jl.garhdez@gmail.com>
2017-05-31 10:23:48 +0200

    move stuff to Hits.Types

--- src/Hits/Core.hs
+++ src/Hits/Core.hs
@@ -1,42 +1,15 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings, DeriveAnyClass #-}
 module Hits.Core (
   status,
   FileChanges
   ) where
 
 import Data.Aeson (ToJSON)
-
 import Data.Attoparsec.Text as AP
-import Data.Text as T
 import Data.Either
-import GHC.Generics
+import Data.Text as T
 import System.Process as P
 
-data Change = Added
-            | Deleted
-            | Modified
-            | Renamed
-            deriving (Eq, Show, Generic, ToJSON)
-
-data FileChanges = FileChanges {
-  changeType :: Change,
-  fileName :: Text
-  } deriving (Show, Eq, Generic, ToJSON)
-
-
---[" D app/Main.hs"," M hits.cabal"," D src/Lib.hs","?? Main.hs","?? src/Hits.hs","?? src/Hits/", "R  asdf -> qwer"]
-
-change :: Parser Change
-change = do
-  ch <- AP.take 2
-  return $ res ch
-    where res "??" = Added
-          res " D" = Deleted
-          res " M" = Modified
-          res "R " = Renamed
-
-fileChange :: Parser FileChanges
-fileChange = FileChanges <$> (change <* AP.char ' ') <*> AP.takeText
+import Hits.Types (FileChanges, fileChange)
   
 
 process :: IO Text
--- src/Hits/Types.hs
+++ src/Hits/Types.hs
@@ -1,1 +1,34 @@
-module Hits.Types() where
+{-# LANGUAGE DeriveGeneric, OverloadedStrings, DeriveAnyClass #-}
+module Hits.Types(
+  FileChanges,
+  fileChange
+  ) where
+
+import Data.Aeson (ToJSON)
+import Data.Text as T
+import Data.Attoparsec.Text as AP
+import GHC.Generics
+
+data Change = Added
+            | Deleted
+            | Modified
+            | Renamed
+            deriving (Eq, Show, Generic, ToJSON)
+
+data FileChanges = FileChanges {
+  changeType :: Change,
+  fileName :: Text
+  } deriving (Show, Eq, Generic, ToJSON)
+
+
+change :: Parser Change
+change = do
+  ch <- AP.take 2
+  return $ res ch
+    where res "??" = Added
+          res " D" = Deleted
+          res " M" = Modified
+          res "R " = Renamed
+
+fileChange :: Parser FileChanges
+fileChange = FileChanges <$> (change <* AP.char ' ') <*> AP.takeText
